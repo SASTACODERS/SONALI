860ee264f38b7fe2593d614de38a1668b51e1f4e
SASTACODERS <princekumar34379@gmail.com>
2024-12-21 10:31:07 +0530

    ᴘᴜʀᴠɪ

--- PURVIMUSIC/utils/inline/play.py
+++ PURVIMUSIC/utils/inline/play.py
@@ -57,7 +57,7 @@ def stream_markup_timer(_, chat_id, played, dur):
         [
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
             InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="⎙", callback_data=f"add_playlist {videoid}"),
+            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
@@ -69,27 +69,13 @@ def stream_markup_timer(_, chat_id, played, dur):
         ],
       [
             InlineKeyboardButton(
-                text="⪻  -10s",
-                callback_data=f"ADMIN 1|{chat_id}",
+                text="𝖮ᴡɴᴇʀ 🥀", url="https://t.me/PURVI_SUPPORT",
             ),
             InlineKeyboardButton(
-                text="↻",
-                callback_data=f"ADMIN Replay|{chat_id}"
-            ),  
-            InlineKeyboardButton(
-                text="+10s  ⪼",
-                callback_data=f"ADMIN 2|{chat_id}",
-            ),
+                text="𝖲ᴜᴘᴘᴏʀᴛ 🥀", url="https://t.me/PURVI_UPDATES",
+            )
         ],
-         [
-            InlineKeyboardButton(
-                text=_["⍟ ᴀᴅᴅ ᴍᴇ ⍟"],
-                url=f"https://t.me/{app.username}?startgroup=true",
-            ),
-            InlineKeyboardButton(
-                text=_["CLOSE_BUTTON"], callback_data="close",
-            ),
-        ],
+         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
 

2886d6bb972fb8b13f9a9d3198b2004e713793d0
SASTACODERS <princekumar34379@gmail.com>
2024-12-21 10:34:13 +0530

    Update queue.py

--- PURVIMUSIC/utils/inline/queue.py
+++ PURVIMUSIC/utils/inline/queue.py
@@ -69,9 +69,17 @@ def aq_markup(_, chat_id):
         [
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
             InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
-        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
-    ]
+        [
+            InlineKeyboardButton(
+                text="𝖠ʟᴘʜᴀ 𝖶ᴏʀʟᴅ 🥀", url=f"https://t.me/WORLD_ALPHA"
+            ),
+            InlineKeyboardButton(
+                text="𝖢ʜᴀᴛ 𝖦ʀᴏᴜᴘ 🥀", url=f"https://t.me/PURVI_UPDATES"
+            ),
+        ],
+      ]
     return buttons
